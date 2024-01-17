4db5809efce7078ad74614f039b6547d66e4801b
BharathiRathna <156513298+BharathiRathna@users.noreply.github.com>
2024-01-17 16:45:08 +0530

    initialcommit

---
+++ src/Framework/Container.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+use ReflectionClass;
+use Framework\Exceptions\ContainerException;
+
+class Container
+{
+ private array $definitions =[];
+
+ public function addDefinitions(array $newDefinitions)
+ {
+   $this->definitions = [...$this->definitions,...$newDefinitions];
+
+   dd($this->definitions);
+ }
+ public function resolve(string $className)
+ {
+   
+
+    $reflectionClass = new ReflectionClass($className);
+
+    if(!reflectionClass->isInstantiable()){
+      throw new ContainerException("Class {$classname} is not instantiable");
+    }
+        dd($reflectionClass);
+ }
+}
